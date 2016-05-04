ebed1d1bbcdf64d4cbe824e3cd4bbcdeb536f571
Merrick Sapsford <merrick@sapsford.tech>
2016-05-04 11:04:17 +0100

    Add view controllers to swift example

---
+++ Example/Swift/Paged Tabs Example/Source/UI/Screens/ChildViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChildViewController.swift
+//  Paged Tabs Example
+//
+//  Created by Merrick Sapsford on 04/05/2016.
+//  Copyright © 2016 Merrick Sapsford. All rights reserved.
+//
+
+import UIKit
+import MSSTabbedPageViewController
+
+class ChildViewController: UIViewController {
+    
+}
---
+++ Example/Swift/Paged Tabs Example/Source/UI/Screens/TabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewController.swift
+//  Paged Tabs Example
+//
+//  Created by Merrick Sapsford on 04/05/2016.
+//  Copyright © 2016 Merrick Sapsford. All rights reserved.
+//
+
+import UIKit
+import MSSTabbedPageViewController
+
+class TabViewController: MSSTabbedPageViewController {
+    
+    override func viewControllersForPageViewController(pageViewController: MSSPageViewController!) -> [AnyObject]! {
+        return nil
+    }
+    
+}
